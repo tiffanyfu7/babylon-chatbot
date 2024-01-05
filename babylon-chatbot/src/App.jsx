9f4c8272597fdec31c88627cd55a332d87502caa
Samuel Glasscock <samuel.c.glasscock@gmail.com>
2024-01-05 15:17:32 -0500

    merge conflict resolution

--- babylon-chatbot/src/App.jsx
+++ babylon-chatbot/src/App.jsx
@@ -1,38 +1,13 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import {
-  createThread,
-  retrieveThread,
-  deleteThread,
-  createMessage,
-  retrieveAssistant,
-} from "./assistant.js";
+
+import { createThread, retrieveAssistant } from "./assistant.js";
 import { InputBar } from "./components/InputBar.jsx";
 import { Links } from "./components/Links.jsx";
 import BabylonLogo from "./images/BabylonLogo.png";
 
 function App() {
-  const [threadID, setThreadID] = useState("");
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    createThread()
-      .then((obj) => {
-        console.log(obj), setThreadID(JSON.stringify(obj.id));
-      })
-      .catch((error) => console.log(error));
-  }, []);
-
-  console.log(threadID);
-
-  // work on this next
-  // useEffect(() => {
-  //   createMessage()
-  //     .then((obj) => {
-  //       console.log(obj), setThreadID(JSON.stringify(obj.id));
-  //     })
-  //     .catch((error) => console.log(error));
-  // }, []);
+  console.log(createThread().then(obj => console.log(obj)));
 
   return (
     <>
