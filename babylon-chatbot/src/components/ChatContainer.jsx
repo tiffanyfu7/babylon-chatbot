e69f8ba1799eb135e6d65322f78d0d02060ed44b
RishiSarraff <sarraff2004@gmail.com>
2024-01-09 14:58:19 -0500

    changes using async await sugar text

--- babylon-chatbot/src/components/ChatContainer.jsx
+++ babylon-chatbot/src/components/ChatContainer.jsx
@@ -1,101 +1,79 @@
 import { useEffect, useState } from "react";
-import { MessageChannel } from "./MessageChannel.jsx"
-import { createThread, createMessage, listMessages, createRun, createResponse } from "../assistant.js";
+import { MessageChannel } from "./MessageChannel.jsx";
+import {
+  createThread,
+  createMessage,
+  listMessages,
+  createRun,
+  createResponse,
+} from "../assistant.js";
 
 export const ChatContainer = () => {
-  let [submittedPrompt, setSubmittedPrompt] = useState("");
+  const [submittedPrompt, setSubmittedPrompt] = useState("");
   const [threadID, setThreadID] = useState("");
-  const [runID, setRunID] = useState("");
-  const [response, setResponse] = useState(null);
-  const [assistantResponse, setAssistantResponse] = useState("");
-  const [messageListData, setMessageListData] = useState([{}]);
   const [messageList, setMessageList] = useState([""]);
 
+  useEffect(() => {
+    createThread()
+      .then((obj) => {
+        setThreadID(String(obj.id));
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-
-  const handleSubmit = (prompt) => {
+   async function handleSubmit(prompt){
     prompt.preventDefault(); // prevents the form from autosubmitting, if you see a question mark at the https part then it is not processing the code
-    let inputOfUser = prompt.target.userInput.value;
+    const inputOfUser = prompt.target.userInput.value;
     setSubmittedPrompt(inputOfUser);
     setMessageList([...messageList, submittedPrompt]);
-
     console.log(messageList);
-  };
 
+    const message = await createMessage(threadID, inputOfUser)
 
-  useEffect(() => {
-    createThread(submittedPrompt !== null)
-      .then((obj) => { setThreadID(String(obj.id)) })
-      .catch((error) => console.log(error));
-  }, [submittedPrompt]);
+    const run = await createRun(threadID)
 
-  useEffect(() => {
-    if (threadID) {
-      createMessage(threadID, submittedPrompt)
-        .then((obj) => { console.log("user message"); console.log(obj); })
-        .catch((error) => console.log(error));      
-    }
-  }, [threadID]);
+    const response = await createResponse(threadID, run.id)
 
-  useEffect(() => {
-    if(threadID){   
-      createRun(threadID)
-      .then((obj) => { setRunID(obj.id) })
-      .catch((error) => console.log(error))
-    }
-  }, [threadID]);
+    const messages = await listMessages(threadID)
 
-  useEffect(() => {
-    if (runID) {
-      createResponse(threadID, runID)
-        .then((obj) => {
-          if (obj !== null) { setResponse(obj); }
-        })
-        .catch((error) => console.log(error));
-    }
-  }, [runID]);
+    console.log(messages)
 
-  useEffect(() => {
-    if (response !== null) {
-      listMessages(threadID)
-        .then((array) => { setMessageListData(array) })
-        .catch((error) => console.log(error));
-    }
-  }, [response]);
+    let responseMessage = messages.filter((obj) => obj.run_id === run.id && obj.role === "assistant")
+    .pop();
 
-  useEffect(() => {
-    if (messageListData !== null) {
-      const responseMessage = messageListData.filter((obj) => obj.run_id === runID && obj.role === "assistant").pop();
-      if(responseMessage){
-        setMessageList([...messageList, (responseMessage.content[0]["text"].value)]);
-      }
-  }
-}, [messageListData]);
+    responseMessage = responseMessage.content[0]["text"].value
+    console.log(responseMessage)
 
+    messageList.push(responseMessage)
 
- 
+   }
   return (
     <div>
       <div>
         {messageList.map((message, index) => {
-          <MessageChannel key={index} message={message} userOrNot={index%2 === 0}/>
+          <MessageChannel
+            key={index}
+            message={message}
+            userOrNot={index % 2 === 0}
+          />;
         })}
       </div>
+
       <form
         className="box"
         method="post"
         onSubmit={(prompt) => handleSubmit(prompt)}
       >
-          <input
-            className="inputbox"
-            type="text"
-            name="userInput"
-            placeholder="Enter prompt here"
-          />
-          <button className="submitButton" >Submit</button>
+        <input
+          className="inputbox"
+          type="text"
+          name="userInput"
+          placeholder="Enter prompt here"
+        />
+        <button type="submit" className="submitButton">
+          Submit
+        </button>
       </form>
-
-      
     </div>
   );
 };
--- babylon-chatbot/src/components/MessageChannel.jsx
+++ babylon-chatbot/src/components/MessageChannel.jsx
@@ -1,8 +1,8 @@
 export const MessageChannel = ({message, userOrNot}) => {
     
     return (
-        <div className={userOrNot ? "User-Box" : "WaterBoy-Box"}>
-            {console.log(message)}
+        <div className={userOrNot ? "WaterBoy-Box" : "User-Box"}>
+            <h1>{message}</h1>
         </div>
     )
 }
