2032ec0494e9e80bbfe2d445a784604f759b8461
Tiffany Fu <tiffany.fu7@gmail.com>
2024-01-10 11:58:32 -0500

    copy button added to assistant messages

--- src/components/MessageChannel.jsx
+++ src/components/MessageChannel.jsx
@@ -1,29 +1,25 @@
-import { lightGreen } from "@mui/material/colors"
-
 export const MessageChannel = ({ message, role }) => {
-    //if message is sent from user, align to right of page (not working)
     if (role === "user") {
         return (
-            <div
-                class="message"
-                // style={{ textAlign: "right", marginRight: "0px" }}
-                style={{ backgroundColor: "#cff4cd"}}
-            >
+            <div class="message" style={{ backgroundColor: "#cff4cd"}} >
                 <h4 style={{ marginBottom: "-15px" }} >You</h4>
                 <p>{message}</p>
             </div>
         )
     }
-    //if message is sent from assistant align left (not working)
     else if(role === "assistant") {
         return (
-            <div
-                class="message"
-                // style={{ textAlign: "left", marginLeft: "0px" }}
-                style={{ backgroundColor: "white"}}
+            <div class="message" style={{ backgroundColor: "white"}}
             >
                 <h4 style={{ marginBottom: "-15px" }}>WaterBoy</h4>
                 <p>{message}</p>
+                { message !== "Fetching a Response..." &&
+                    <button id="copy-button"
+                        onClick={() => navigator.clipboard.writeText(message)}
+                    >
+                        Copy
+                    </button >
+                }
             </div>
         )
     }
